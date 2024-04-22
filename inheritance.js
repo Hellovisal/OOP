d57075d0c85be9f2f24eb6900e3b698d6b2ba910
Hello Visal <1220.torn.visal@rupp.edu.kh>
2024-04-22 09:29:26 +0700

    git push

--- inheritance.js
+++ inheritance.js
@@ -1,25 +1,4 @@
-//================= [Encapsolution] ===========
-class Employee{
-    setEmp(name,id,gender){
-        this.name=name;
-        this.id=id;
-        this.gender=gender;
-    }
-    getEmpName(){
-        return this.name;
-    }
-    getEmpId(){
-        return this.id;
-    }
-    getEmpGender(){
-        return this.gener;
-    }
 
-}
-let emp1=new Employee();
-    emp1.setEmp('SAL',"001","M");
-    console.log(emp1.getEmpName());
-    console.log(emp1.getEmpId());
 // ================ [inheritance] ===========
 class animal{
     eat(){
@@ -31,24 +10,3 @@ class dog extends animal{
 }
 const dogs=new dog();
 dogs.eat();
-// ====================[Polymorphism]=================
-class Animal{
-    constructor (name){
-        this.name=name;
-    }
-    sounds(){
-        console.log("Generic Animal sound!");
-    }
-}
-class dog extends Animal{
-    constructor (name){
-        super(name);
-    }
-    sounds(){
-        console.log("woof! woof!");
-    }
-}
-const a1=new Animal("Dom");
-const a2=new dog("Jelly");
-a1.sounds();
-a2.sounds();
